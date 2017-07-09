e5bd60cf379347913951a3bed65ec214f2f96ea5
Ezekiel Saturday <Ezekiel Saturday>
2017-07-09 07:04:40 +0100

    added 3rd party API

--- expressApp/src/services/goodreadsServices.js
+++ expressApp/src/services/goodreadsServices.js
@@ -1,9 +1,32 @@
 /*jslint node: true*/
+var http =  require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser({explicitArray: false});
+
 module.exports = function () {
 	
 	var getById = function (id, cb) {
 			'use strict';
-		    cb(null, {description: "our description goes here"});
+			var options = {
+				host: 'www.goodreads.com',
+				path: '/book/show/656?format=xml&key=CAnaz9O6AUjlNh5RStpQ7A'
+			};
+			
+		    var callback= function(response){
+				var str = '';
+				
+				response.on('data', function(chunk){
+					str += chunk;
+				});
+				response.on('end', function () {
+					console.log(`${str} with love 'goodreads'`);
+					parser.parseString(str, 
+					     function (err, result) {
+						cb(null, result.GoodreadsResponse.book);
+					});
+				});
+			}
+			http.request(options, callback).end();
 	    };
 	/*==using reveal module pattern to controll the export of the function==*/
 	return {
